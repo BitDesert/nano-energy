ea412d4d5788263e40f63f89eb7be651fd72cc9e
Tobias Palmer <tobiaspalmer93@gmail.com>
2018-11-22 19:03:00 +0100

    first test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <router-view/>
+    
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  padding-top: 5rem;
+}
+.starter-template {
+  padding: 3rem 1.5rem;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main role="main" class="container">
+
+    <h1>Nano Live Energy Usage</h1>
+
+    <div class="row mb-3">
+      <div class="col-4">
+          <div class="card">
+              <div class="card-body">
+                  <h5 class="card-title">{{blockCount}} Blocks</h5>
+                  <h6 class="card-subtitle text-muted">Current Blockcount</h6>
+              </div>
+          </div>
+      </div>
+    </div>
+
+  </main>
 </template>
 
 <script>
+import axios from "axios";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      blockCount: Number
+    };
+  },
+  methods: {
+    getBlockCount: function() {
+      axios.get("https://api.nanocrawler.cc/block_count").then(
+        response => {
+          this.blockCount = response.data.blockCount.count;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  },
+  mounted: function() {
+    this.getBlockCount();
+    setInterval(this.getBlockCount, 1500);
   }
-}
+};
 </script>
